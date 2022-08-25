3093ad7c7f058ecda0ee41f1635f1b1a55a38973
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-24 22:49:02 -0400

    inital commit for prompt.js

---
+++ Prompts.js
@@ -0,0 +1,21 @@
+// What is the keyword that displays a box requesting user input?
+
+prompt
+
+// What is the keyword in a statement that shares most of the syntax of a prompt statement?
+
+alert
+
+// What do you need in order to capture a user's input in a prompt box? Answer with one word.
+
+variable
+
+var gender = prompt(askGender, bestGuess);
+// In the above statement, what is optional?
+
+var bestGuess;
+
+// Code a prompt with the message "Enter first name". The user's response is assigned to firstName, which hasn't been declared beforehand.
+
+var firstName = prompt ("Enter first name");
+
